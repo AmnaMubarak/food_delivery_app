ed7a523ef8084779f79f5a48d21eb5be9bd58ce7
Amna Mubarak <amnamubarakk111@gmail.com>
2023-11-02 20:40:07 +0500

    navbar added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
 import Home from './screens/Home';
+// import Login from './screens/Login';
+
+import {
+  createBrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 function App() {
   return (
-  <>
- <Home />
-  </>
+  <Router>
+    
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+
+    </div>
+  </Router>
   );
 }
 
---
+++ myapp/src/screens/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      amna
+    </div>
+  )
+}
+
+export default Login
