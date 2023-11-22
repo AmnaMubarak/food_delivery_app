82afa40b782214ef386a94efa033031f8bea1695
Amna Mubarak <amnamubarakk111@gmail.com>
2023-11-22 23:58:22 +0500

    few changes added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
 import Login from './screens/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <Routes>
+    //       <Route path="/" element={<Navbar/>} />
+    //       <Route path="/login" element={<Login />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+    
+      <Home />
+
   );
 }
 
