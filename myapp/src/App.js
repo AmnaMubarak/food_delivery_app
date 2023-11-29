7a4c79d3faf71d3179b8454130a562aa147ddeb8
Amna Mubarak <amnamubarakk111@gmail.com>
2023-11-29 21:30:17 +0500

    few changes added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './screens/Home';
-import Login from './screens/Login';
-import Navbar from './components/Navbar';
+//import Login from './screens/Login';
 
 function App() {
   return (
     // <Router>
-    //   <div>
-    //     <Routes>
-    //       <Route path="/" element={<Navbar/>} />
-    //       <Route path="/login" element={<Login />} />
-    //     </Routes>
-    //   </div>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
     // </Router>
-    
-      <Home />
-
+    <Home />
   );
 }
 
--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-       <nav className=" sticky bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <div className='mb-32'>
+       <nav className="h-32 mb-32 first-letter:sticky bg-blue-300 dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GoFood</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
          
@@ -27,14 +27,14 @@ export default function Navbar() {
         {/* <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky"> */}
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row ">
             <li>
-              <a href="#" className="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">
+              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">
                 Home
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
               </a>
@@ -42,7 +42,7 @@ export default function Navbar() {
             <li>
               <a
                 href="/login"
-                className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Login
               </a>
